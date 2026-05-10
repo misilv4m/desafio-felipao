a03262443691c72e0918ab9f467f9aff046fa15d
mavik silva <milenasccpsilva.30@gmail.com>
2026-05-10 00:13:15 -0300

    corrigi xp

--- src/index.js
+++ src/index.js
@@ -10,17 +10,17 @@ let nivelHeroi = "";
 //Estrutura de decisão para determinar o nível do herói com base na quantidade de XP
 if (xpHeroi <= 1000) {
     nivelHeroi = "Ferro";
-} else if (xpHeroi >= 1001 && xp <= 2000) {
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
     nivelHeroi = "Bronze";
-} else if (xpHeroi >= 2001 && xp <= 5000) {
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
     nivelHeroi = "Prata";
-} else if (xpHeroi >= 5001 && xp <= 7000) {
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
     nivelHeroi = "Ouro";
-} else if (xpHeroi >= 7001 && xp <= 8000) {
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
     nivelHeroi = "Platina";
-} else if (xpHeroi >= 8001 && xp <= 9000) {
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
     nivelHeroi = "Ascendente";
-} else if (xpHeroi >= 9001 && xp <= 10000) {
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
     nivelHeroi = "Imortal";
 } else {
     nivelHeroi = "Radiante";
